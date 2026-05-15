72b70c13f4965cd14b338208f4269df4499f03bd
Akshaya-cpu <sunkaraakshaya41@gmail.com>
2026-05-15 12:50:04 +0530

    remove crashing options route

--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,30 @@ const cors = require("cors");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-// CORS FIX
-app.use(
-  cors({
-    origin: [
-      "https://team-task-manager-kaj8.vercel.app",
-      "https://team-task-manager-kaj8-fqbzncho8-akshaya-s-projects4.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-
-app.options("*", cors());
+app.use(cors({
+  origin: [
+    "https://team-task-manager-kaj8.vercel.app",
+    "https://team-task-manager-kaj8-fqbzncho8-akshaya-s-projects4.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 
 app.use(express.json());
-
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/projects", projectRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 8080;
 
